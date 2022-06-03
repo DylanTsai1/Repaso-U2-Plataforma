23969651501de9d53f0908138e01ed94f8282895
DylanTsai1 <101568962+DylanTsai1@users.noreply.github.com>
2022-06-03 08:04:52 -0300

    Initial commit

---
+++ Assets/EggMaker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EggMaker : MonoBehaviour
+{
+    public GameObject eggPreFab;
+    public GameObject eggSpawnPoint; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject clon; 
+            clon = Instantiate(eggPreFab);
+            clon.transform.position = eggSpawnPoint.transform.position; 
+        }
+    }
+}
